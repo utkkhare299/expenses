c953779fb74aafe64809c67e0e83371be20c2e56
utkarsh khare <utkkhare999@gmail.com>
2023-01-27 23:04:37 +0530

    first commit2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Signup from './components/Auth/Signup'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+        <Signup />
+    </div>
+  )
+}
+
+export default App
