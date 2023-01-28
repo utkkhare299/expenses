ea482aac1eec484ced2abfc13fe1e5c18debd923
utkarsh khare <utkkhare999@gmail.com>
2023-01-28 12:37:41 +0530

    signup

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -1,10 +1,10 @@
-import { useState, useRef } from 'react';
-import classes from './auth.module.css';
+import { useState, useRef } from "react";
+import classes from "./auth.module.css";
 
 const Signup = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
-  const passConfirmRef = useRef()
+  const passConfirmRef = useRef();
 
   // const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -21,30 +21,29 @@ const Signup = () => {
     const passConfirm = passConfirmRef.current.value;
 
     let url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBLAZfI3knkbyxNuEyi2t-QrjiOXbPCZVc';
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCCBbOKNdCKFTzfFaVssnidTzE7FXWbWxM";
 
-    if (enteredEmail != '' && enteredPassword != '' && passConfirm != '') {
-
+    if (enteredEmail != "" && enteredPassword != "" && passConfirm != "") {
       setIsLoading(true);
 
       fetch(url, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify({
           email: enteredEmail,
           password: enteredPassword,
           returnSecureToken: true,
         }),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       })
         .then((res) => {
           setIsLoading(false);
           if (res.ok) {
-             console.log('User has successfully signed up')
+            console.log("User has successfully signed up");
           } else {
             return res.json().then(() => {
-              let errorMessage = 'Signup failed'
+              let errorMessage = "Signup failed";
 
               throw new Error(errorMessage);
             });
@@ -53,49 +52,51 @@ const Signup = () => {
         .catch((err) => {
           alert(err.message);
         });
-      }
-    };
-
-    return (
-      <section className={classes.auth}>
-        <h1>Sign Up</h1>
-        <form onSubmit={submitHandler}>
-          <div className={classes.control}>
-            <label htmlFor='email'>Your Email</label>
-            <input type='email' id='email' required ref={emailInputRef} />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Your Password</label>
-            <input
-              type='password'
-              id='password'
-              required
-              ref={passwordInputRef}
-            />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='passConfirm'>Confirm Password</label>
-            <input
-              type='password'
-              id='passConfirm'
-              required
-              ref={passConfirmRef}
-            />
-          </div>
-          <div className={classes.actions}>
-          
-            {isLoading && <p>Sending request...</p>}
-            <button
-              type='submit'
-              className={classes.toggle}
-              onClick={submitHandler}
-            >
-              Create new account
-            </button>
-          </div>
-        </form>
-      </section>
-    );
+    }
   };
 
-  export default Signup;
+  return (
+    <section className={classes.auth}>
+      <h1>Sign Up</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+
+        <div className={classes.control}>
+          <label htmlFor="passConfirm">Confirm Password</label>
+          <input
+            type="password"
+            id="passConfirm"
+            required
+            ref={passConfirmRef}
+          />
+        </div>
+
+        <div className={classes.actions}>
+          {isLoading && <p>Sending request...</p>}
+          <button
+            type="submit"
+            className={classes.toggle}
+            onClick={submitHandler}
+          >
+            Create new account
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Signup;
