e7bc35c27deeb5138a34117ec68ca611836c8856
utkarsh khare <utkkhare999@gmail.com>
2023-01-29 11:54:57 +0530

    verify user

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/authContext";
 import UpdateProfile from "./Auth/UpdateProfile";
 
 function Home() {
   const [show, setShow] = useState(false);
+  const { token } = useContext(AuthContext);
+
+  const handleClick = async () => {
+    const url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBLAZfI3knkbyxNuEyi2t-QrjiOXbPCZVc";
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        requestType: "VERIFY_EMAIL",
+        idToken: token,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await fetch(url, options);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
       <div
@@ -16,6 +40,7 @@ function Home() {
         {/* <p>
           Your Profile is Incomplete. <Link to="/update">Complete Now</Link>
         </p> */}
+        <button onClick={handleClick}>Verify Email</button>
         <p>
           Your Profile is Incomplete.{" "}
           <a href="#" onClick={() => setShow((prev) => !prev)}>
